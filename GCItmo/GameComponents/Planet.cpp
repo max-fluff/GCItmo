106daccfedc0d024afdd6ed6fd0f8777e01ebaec
max-fluff <48261468+max-fluff@users.noreply.github.com>
2023-03-06 21:12:15 +0300

    Add files via upload

--- GCItmo/GameComponents/Planet.cpp
+++ GCItmo/GameComponents/Planet.cpp
@@ -1,11 +1,12 @@
 #include "Planet.h"
 
 #include "../Game.h"
+#include "../DXSDK/SimpleMath.h"
 
 class Game;
 
 Planet::Planet(Game* game, float radius, float axisAngle = 3.0f, float orbitAngle = 0.5f,
-               float orbitRadius = 0.0f, Planet* parent = nullptr)
+               float orbitRadius = 0.0f, DirectX::SimpleMath::Vector3 axisVector = DirectX::SimpleMath::Vector3(0.0f,0.0f,0.0f),  Planet* parent = nullptr)
 {
 	this->game = game;
 	this->axeAngle = axisAngle;
@@ -13,24 +14,36 @@ Planet::Planet(Game* game, float radius, float axisAngle = 3.0f, float orbitAngl
 	this->orbitR = orbitRadius;
 	this->r = radius;
 	this->parent = parent;
+	this->axisVector = axisVector;
 }
 
 void Planet::Draw()
 {
-	modelMat = rotationAxisMat * DirectX::XMMatrixTranslation(orbitR, 0.0f, 0.0f) * rotationOrbitMat;
 
-	data.worldViewProj = modelMat * localCoordMat *
-		game->camera->GetViewMatrix() *
-		game->camera->GetProjectionMatrix();
 }
 
 void Planet::Update(float deltaTime)
 {
+
+/*
 	rotationAxisMat *= DirectX::XMMatrixRotationY(axeAngle * 0.01f);
 	rotationOrbitMat *= DirectX::XMMatrixRotationY(orbitAngle * 0.01f);
+	modelMat = rotationAxisMat * DirectX::XMMatrixTranslation(orbitR, 0.0f, 0.0f) * rotationOrbitMat;
 
 	if (parent)
-		localCoordMat = DirectX::XMMatrixTranslation(parent->orbitR, 0.0f, 0) * parent->rotationOrbitMat;
+		localCoordMat = DirectX::XMMatrixTranslation(parent->orbitR, 0.0f, 0) * rotationOrbitMat;*/
+
+	rotationAxisMat *= DirectX::XMMatrixRotationY(axeAngle * 0.01f);
+	rotationOrbitMat *= DirectX::XMMatrixRotationAxis(axisVector,orbitAngle * 0.01f);
+
+	modelMat = rotationAxisMat * DirectX::XMMatrixTranslation(orbitR, 0.0f, 0.0f) * rotationOrbitMat;
+
+	if (parent)
+		modelMat *= DirectX::SimpleMath::Matrix::CreateTranslation(parent->modelMat.Translation());
+
+	data.worldViewProj = modelMat * localCoordMat *
+		game->camera->GetViewMatrix() *
+		game->camera->GetProjectionMatrix();
 }
 
 void Planet::Init()
--- GCItmo/GameComponents/Planet.h
+++ GCItmo/GameComponents/Planet.h
@@ -3,25 +3,24 @@
 #include "../GameComponent.h"
 #include "../DXSDK/ConstantBufferTypes.h"
 #include "../GameComponents/SphereObject.h"
-
-#include <DirectXMath.h>
+#include "../DXSDK/SimpleMath.h"
 
 class Game;
 
 class Planet : public GameComponent
 {
 public:
-	Planet(Game* game, float radius, float axisAngle, float orbitAngle, float orbitRadius, Planet* parent);
+	Planet(Game* game, float radius, float axisAngle, float orbitAngle, float orbitRadius, DirectX::SimpleMath::Vector3 axisVector, Planet* parent);
 	void Draw() override;
 	void Update(float deltaTime) override;
 	void Init() override;
 
 	CB_VS_vertexshader data;
 
-	DirectX::XMMATRIX modelMat = DirectX::XMMatrixIdentity();
-	DirectX::XMMATRIX rotationOrbitMat = DirectX::XMMatrixRotationY(0.0f);
-	DirectX::XMMATRIX rotationAxisMat = DirectX::XMMatrixRotationY(0.0f);
-	DirectX::XMMATRIX localCoordMat = DirectX::XMMatrixIdentity();
+	DirectX::SimpleMath::Matrix modelMat = DirectX::XMMatrixIdentity();
+	DirectX::SimpleMath::Matrix rotationOrbitMat = DirectX::XMMatrixRotationY(0.0f);
+	DirectX::SimpleMath::Matrix rotationAxisMat = DirectX::XMMatrixRotationY(0.0f);
+	DirectX::SimpleMath::Matrix localCoordMat = DirectX::XMMatrixIdentity();
 
 	float orbitAngle = 1.0f;
 	float axeAngle = 3.0f;
@@ -31,4 +30,6 @@ public:
 private:
 	Planet* parent;
 	Game* game;
+
+	DirectX::SimpleMath::Vector3 axisVector;
 };
